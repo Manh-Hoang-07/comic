19834d88477cd56b445c93660dce3aeeb8ed7fc2
Manh-Hoang-07 <vumanhhoang140799@gmail.com>
2026-04-14 00:57:31 +0700

    update build

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,4 @@
 import 'reflect-metadata';
-import * as moduleAlias from 'module-alias';
-moduleAlias.addAliases({ '@': __dirname });
 import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
 import { NestExpressApplication } from '@nestjs/platform-express';
